29ffa97bf0bf4c08773a8234c0a5f4f99edbddc7
Matias Barcenas <MPX.4132@Gmail.com>
2017-04-14 16:16:15 -0500

    Loop detection & moved scripts
    Implemented an algorithm to properly detect a loop in the game by using
    an undirected graph approach.
    Moved the scripts to scripts.js.

---
+++ scripts.js
@@ -0,0 +1,227 @@
+function Neighbors($target) {
+	let $column = $target.parent();
+	let $columnPrev = $column.prev();
+	let $columnNext = $column.next();
+	let offset = $column.hasClass("offset");
+	let currentIndex = $target.index();
+	
+	let $columnChildren = $column.children();
+	
+	var neighbors = new Array();
+	
+	let indexOffset = (offset? 0 : -1);
+	
+	
+	if (currentIndex - 1 >= 0) neighbors.push($columnChildren[currentIndex - 1]);
+	else neighbors.push(null);
+	
+	if ($columnNext.length) {
+		let $columnNextChildren = $columnNext.children();
+		
+		if ($columnNextChildren[currentIndex + indexOffset]) neighbors.push($columnNextChildren[currentIndex + indexOffset]);
+		else neighbors.push(null);
+		
+		if ($columnNextChildren[currentIndex + 1 + indexOffset]) neighbors.push($columnNextChildren[currentIndex + 1 + indexOffset]);
+		else neighbors.push(null);
+		
+	} else {
+		neighbors.push(null);
+		neighbors.push(null);
+	}
+	
+	if (currentIndex + 1 < $columnChildren.length) neighbors.push($columnChildren[currentIndex + 1]);
+	else neighbors.push(null);
+	
+	if ($columnPrev.length) {
+		let $columnPrevChildren = $columnPrev.children();
+		
+		if ($columnPrevChildren[currentIndex + 1 + indexOffset]) neighbors.push($columnPrevChildren[currentIndex + 1 + indexOffset]);
+		else neighbors.push(null);
+		
+		if ($columnPrevChildren[currentIndex + indexOffset]) neighbors.push($columnPrevChildren[currentIndex + indexOffset]);
+		else neighbors.push(null);
+		
+	} else {
+		neighbors.push(null);
+		neighbors.push(null);
+	}
+	
+	return neighbors;
+}
+
+function IsValidPlayerPieceVertex(player, $piece, vertex) {
+	if (!($piece instanceof jQuery)) return false;
+	if (["L", "R"].indexOf(player) < 0) return false;
+	if (vertex < 0 && vertex > 2) return false;
+	return true;
+}
+
+function GetPlayerPieceVertex(player, $piece, vertex) {
+	if (!IsValidPlayerPieceVertex(player, $piece, vertex)) return false;
+	return $piece.data(player)[vertex];
+}
+
+function SetPlayerPieceVertexValue(player, $piece, vertex, value) {
+	if (!IsValidPlayerPieceVertex(player, $piece, vertex)) return false;
+	return $piece.data(player)[vertex] = value;
+}
+
+function GetLinkedNode(node) {
+	return GetPlayerPieceVertex(node.targetPlayerID, node.targetPiece, node.targetNodeID);
+}
+
+function HasCircuit(vertex) {
+	var visited = new Array();
+	
+	while (vertex) {
+		// It doesn't have two connections, it's not part of a circuit.
+		if (vertex.length < 2) return false;
+		
+		// Visit the current vertex, by getting source from an edge
+		// Notice that they're both coming form the same vertex, meaning,
+		// source is the same on vertex[0] and vertex[1]...
+		visited.push(vertex[0].source);
+		
+		var found = false;
+		for (var edgeID in vertex) {
+			if (visited.indexOf(vertex[edgeID].destination) < 0) {
+				vertex = vertex[edgeID].destination.data("edge")[vertex[edgeID].vertexID];
+				found = true;
+				break;
+			}
+		}
+		
+		if (!found) return true; // If there exists no unvisited node, we've got a loop!
+	}
+
+	return false
+}
+
+function IsClosed($target) {
+	for (var thisVertexID in $target.data("edge")) {
+		if (HasCircuit($target.data("edge")[thisVertexID])) return true;
+	}
+
+	return false;
+}
+
+function VertexIDForFaceID($piece, faceID) {
+	let rotation = {0: 0, 120: 1, 240: 2}[$piece.data("rotation")];
+	let map = [[[0, 1], [2, 5], [3, 4]], [[2, 3], [1, 4], [0, 5]], [[4, 5], [0, 3], [1, 2]]][rotation];
+	
+	for (var i = 0; i < map.length; i++) {
+		if (map[i].indexOf(faceID) >= 0) return i;
+	}
+}
+
+$(function() {
+	let $controls = $($("#t-controls").html());
+	var $pieceLastCommitted = null;
+	var $pieceLastSelected = null;
+	
+	var PieceCount = 0;
+	
+	function $MakeItem() {
+		var $item = $("<li>").click(function() {
+			if ($pieceLastSelected && !$pieceLastSelected.data("committed"))
+				$pieceLastSelected.removeClass("used");
+			
+			if ($(this).data("committed")) return;
+			$(this).data("committed", false)
+			
+			$pieceLastSelected = $(this);
+			
+			$controls.css("transform", "rotate(" + ($(this).data("rotation")? ($(this).data("rotation") * -1) : 0) + "deg)");
+			$(this).addClass("used").append($controls);
+			
+
+			$controls.show();
+		});
+		
+		$item.data("edge", [new Array(), new Array(), new Array()]);
+		$item.data("rotation", 0);
+		
+		return $item;
+	}
+	
+	$controls.find("a.rotation").click(function() {
+		let $parent = $(this).closest("li");
+		let currentRotation = $parent.data("rotation") || 0;
+		let offsetRotation = 120;
+		let rotation = (currentRotation + offsetRotation) % 360;
+		$parent.css("transform", "rotate(" + rotation + "deg)");
+		$parent.data("rotation", rotation);
+		$parent.children().each(function(i, child) {
+			$(child).css("transform", "rotate(" + (-1 * $parent.data("rotation")) + "deg)")
+		});
+	})
+	
+	$controls.find("a.commit").click(function() {
+		let $target = $(this).closest("li");
+		let $parent = $target.parent();
+		let neighbors = Neighbors($target);
+		
+		$pieceLastCommitted = $target;
+		
+		$target.data("committed", true);
+		
+		if ($("body > div#board > ul:first-child").is($parent)) {
+			var $list = $("<ul>");
+			if (!$parent.hasClass("offset")) $list.addClass("offset");
+			$parent.children().each(function(i) {
+				$list.append($MakeItem());
+			})
+			$("body > div#board").prepend($list);
+		}
+		if ($("body > div#board > ul:last-child").is($parent)) {
+			var $list = $("<ul>");
+			if (!$parent.hasClass("offset")) $list.addClass("offset");
+			$parent.children().each(function(i) {
+				$list.append($MakeItem());
+			})
+			$("body > div#board").append($list);
+		}
+		if ($($parent.children().first()).is($target)) {
+			$("body > div#board > ul").each(function(i) {
+				$(this).prepend($MakeItem());
+			});
+		}
+		if ($($parent.children().last()).is($target)) {
+			$("body > div#board > ul").each(function(i) {
+				$(this).append($MakeItem());
+			});
+		}
+		
+		var date = new Date();
+		console.log(date.getTime());
+		
+		// Attach line between neighboring tiles
+		for (var thisFaceID = 0; thisFaceID < neighbors.length; thisFaceID++) {
+			let $neighbor = neighbors[thisFaceID] && $(neighbors[thisFaceID]);
+			
+			// We only setup existing neighbors that are in use, skip the rest.
+			if (!$neighbor || !$neighbor.hasClass("used")) continue;
+			
+			// Determine our neighbor's face facing us (neighbor side connecting us).
+			let thatFaceID = (thisFaceID + 3) % 6;
+			
+			// Determine the vertex we're dealing with, based on the face of the pieces.
+			// These are different since different faces connect to different vertices.
+			let thisVertexID = VertexIDForFaceID($target, thisFaceID);
+			let thatVertexID = VertexIDForFaceID($neighbor, thatFaceID);
+			
+			// Push new edges to both pieces.
+			$target.data("edge")[thisVertexID].push({source:$target, destination:$neighbor, vertexID:thatVertexID});
+			$neighbor.data("edge")[thatVertexID].push({source:$neighbor, destination:$target, vertexID:thisVertexID});
+		}
+		
+		$target.append($("<p>").text(++PieceCount));
+		
+		if (IsClosed($target)) console.log("Loop detected!");
+		
+		$controls.hide();
+	})
+
+	var $StartList = $("<ul>").append($MakeItem());
+	$("body > div#board").append($StartList);
+});
